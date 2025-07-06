e50db5f331af3c31b559b482cc8189bd318f0131
Tim Della-Chiesa Pasquer <tim.dellachiesapasquer@gmail.com>
2025-07-06 17:26:12 +0200

    ajout custom id et fin creator

--- modules/mazeIndex.js
+++ modules/mazeIndex.js
@@ -7,4 +7,67 @@ async function getNextMaxId() {
     return [idList.reduce((acc, cur) => acc === cur ? acc + 1 : acc, 1), idList[idList.length - 1]]
 }
 
-module.exports = {getNextMaxId};
+async function getRandomId(num, alreadyList) { //Returns num different idCustoms not in the alreadyList, assuming all alreadyList ids are real
+    const data = await Maze.find().select('idCustom');
+    idList = data.map(element => element.idCustom);
+    idList.sort();
+    const n = idList.length - alreadyList.length;
+    alreadyList = alreadyList.sort();
+    const randomList = getRandomNums(n, num);
+    getFirstWithoutSecond(idList, alreadyList);
+    return randomList.map(element => idList[element]);
+}
+
+function getFirstWithoutSecond(array1, array2) {//Assuming array1 is sorted, return a new array with the values of array1 - those of array2
+    for (const element of array2) {
+        console.log(element, array1, array2)
+        array1.splice(findFast(element, array1, 0, array1.length), 1);
+    }
+}
+
+function random(n) { //Returns a random integer between 0 and n - 1
+    return Math.floor(Math.random() * n);
+}
+
+function insertFast(m, numberList, a, b) {
+    if (b === a) {
+        numberList.splice(a, 0, m);
+        return;
+    }
+    const c = Math.floor((b - a) / 2) + a;
+    return numberList[c] < m ? insertFast(m, numberList, c + 1, b) : insertFast(m, numberList, a, c);
+}
+
+function findFast(m, numberList, a, b) {
+    if (b - a === 1) {
+        return a;
+    }
+    const c = Math.floor((b - a) / 2) + a;
+    return numberList[c] <= m ? findFast(m, numberList, c, b) : insertFast(m, numberList, a, c);
+}
+
+function getRandomNums(n, m) {//Return m different random integer between 0 and n-1
+    m = Math.min(n, m);
+    const firstArray = Array.from(Array(m).keys(), x => random(n - x));
+    const secondArray = [];
+    for (let i = 0; i < m; i++) {
+        const intermediaryArray = secondArray.map((element, index) => index ? element - secondArray[index - 1] - 1 : element);
+        let current = 0;
+        let index = 0;
+        while (current <= firstArray[i]) {
+            if (index >= secondArray.length) {
+                current = firstArray[i] + 1;
+                continue;
+            }
+            if (secondArray[index] === current + index) {
+                index++;
+            } else {
+                current = Math.min(firstArray[i] + 1, secondArray[index] - index);
+            }
+        }
+        insertFast(current + index - 1, secondArray, 0, secondArray.length);
+    }
+    return secondArray;
+}
+
+module.exports = {getNextMaxId, getRandomId};
--- routes/maze.js
+++ routes/maze.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const {body, param, validationResult} = require('express-validator');
 const {authenticateToken} = require('../modules/jwt');
 const {isGrid, paramsValid} = require('../modules/mazeAlgo');
-const {getNextMaxId} = require('../modules/mazeIndex');
+const {getNextMaxId, getRandomId} = require('../modules/mazeIndex');
 const Maze = require('../models/mazes');
 const User = require('../models/users');
 const mongoose = require('mongoose');
@@ -26,7 +26,7 @@ router.post('/', authenticateToken,
         const {hideWalls, hidePath, hideExit} = req.body.params;
         const newMaze = new Maze({grid: req.body.grid, hideWalls, hidePath, hideExit, creator: user._id, idCustom: getNextMaxId()[0]});
         const test = await newMaze.save();
-        console.log(test._id, test._id.toString());
+        await User.findByIdAndUpdate(user._id, {$push: {mazeList: test._id}})
         res.json({result: true, data: test});
     } catch (error) {
         console.log(error)
@@ -38,7 +38,7 @@ function isMazeList(data) {
     if (!Array.isArray(data)) {
         return false;
     }
-    return !data.some(element => typeof element !== 'string' || !mongoose.Types.ObjectId.isValid(element));
+    return !data.some(element => typeof element !== 'number');
 }
 
 router.post('/random',
@@ -49,14 +49,12 @@ router.post('/random',
         if (!errors.isEmpty()) {
             return res.status(400).json({error: errors.array()});
         }
+        const mazeIdList = await getRandomId(10, req.body.mazeList);
         const mazeList = await Maze.aggregate([
             {
                 $match: {
-                    _id: {$nin: req.body.mazeList.map(element => new mongoose.Types.ObjectId(element))}
+                    idCustom: {$in: mazeIdList}
                 }
-            },
-            {
-                $sample: {size: 10}
             },
             {
                 $lookup: {
@@ -92,8 +90,57 @@ router.post('/random',
 
 
 router.get('/test', async (req, res) => {
-    res.json({test: await getNextMaxId()})
+    res.json({test: await getRandomId(10, [])})
 })
+
+function isNumber(value) {
+    return Number(value) !== NaN;
+}
+
+router.get('/custom/:id', 
+    param('id').isString().custom(isNumber),
+    async (req, res, next) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({error: errors.array()});
+            }
+            const maze = await Maze.aggregate([
+                {
+                    $match: {idCustom: Number(req.params.id)}
+                },
+                {
+                    $lookup: {
+                        from: "users",
+                        localField: "creator",
+                        foreignField: "_id",
+                        as: "creatorInfo"
+                    }
+                },
+                {
+                    $addFields: {
+                        'creatorUsernameArray': '$creatorInfo.username'
+                    }
+                },
+                {
+                    $addFields: {
+                        'creatorUsername': {$arrayElemAt: ['$creatorUsernameArray', 0]}
+                    }
+                },
+                {
+                    $project: {
+                        creatorInfo: 0,
+                        creatorUsernameArray: 0
+                    }
+                }
+            ])
+            return res.json(Boolean(maze) ? {result: true, maze: maze[0]} : {result: false});
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json({result: false, error: 'Erreur du serveur'});
+        }
+    }
+)
 
 router.get('/:id', 
     param('id').isString().escape().isLength({max: 50}),
